2db3aa59aeaef0fbd2d384eb8f2a173638452d02
nishizawamotoki <sokomichi.hizawa@gmail.com>
2025-06-01 11:53:46 +0900

    Enumerable#sumを使ってコードを簡素化した

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -18,29 +18,25 @@ scores.each do |shot|
 end
 frames << [*tmp_frame]
 
-point = 0
-frames.each_with_index do |frame, i|
+point = frames.each_with_index.sum do |frame, i|
   next_frame = frames[i + 1]
   frame_after_next = frames[i + 2]
 
-  if i == 9
-    point += frame.sum
-    break
-  end
+  next frame.sum if i == 9
 
-  point +=
+  frame.sum +
     if frame[0] == 10 && next_frame[0] == 10
       if i == 8
-        20 + next_frame[1]
+        10 + next_frame[1]
       else
-        20 + frame_after_next[0]
+        10 + frame_after_next[0]
       end
     elsif frame[0] == 10
-      10 + next_frame[0] + next_frame[1]
+      next_frame[0] + next_frame[1]
     elsif frame.sum == 10
-      10 + next_frame[0]
+      next_frame[0]
     else
-      frame.sum
+      0
     end
 end
 puts point

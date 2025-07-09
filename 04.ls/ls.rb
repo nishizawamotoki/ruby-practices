1f8cae5368574abcb9a0b5d52ef1c1a44fee5cca
nishizawamotoki <sokomichi.hizawa@gmail.com>
2025-07-09 20:42:58 +0900

    出力行の配列を生成する部分を関数化した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,16 +9,19 @@ def calculate_col_width(filenames, multiples = MULTIPLES_FOR_COLUMN_WIDTH)
   (longest_filename.length / multiples + 1) * multiples
 end
 
+def generate_output_rows(filenames, max_row, col_width)
+  output_rows = Array.new(max_row) { '' }
+  filenames.each_with_index do |filename, filename_index|
+    row_index = filename_index % max_row
+    output_rows[row_index] = output_rows[row_index] + filename.ljust(col_width)
+  end
+  output_rows
+end
+
 filenames = Dir.glob('*')
 exit if filenames.empty?
 
 max_row = filenames.length.ceildiv(COLUMN_COUNT)
 col_width = calculate_col_width(filenames)
 
-output_rows = Array.new(max_row) { '' }
-filenames.each_with_index do |filename, filename_index|
-  row_index = filename_index % max_row
-  output_rows[row_index] = output_rows[row_index] + filename.ljust(col_width)
-end
-
-puts output_rows.join("\n")
+puts generate_output_rows(filenames, max_row, col_width).join("\n")

382b4b78ce83612d14f465f85185375e67c2a681
nishizawamotoki <sokomichi.hizawa@gmail.com>
2026-06-18 19:18:37 +0900

    インスタンス変数をクラス外部から参照できないようにした

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,12 +3,18 @@
 require_relative 'shot'
 
 class Frame
-  attr_reader :first_shot, :second_shot
-
   def initialize(first_mark, second_mark = nil, third_mark = nil)
     @first_shot = Shot.new(first_mark)
     @second_shot = second_mark ? Shot.new(second_mark) : nil
     @third_shot = third_mark ? Shot.new(third_mark) : nil
+  end
+
+  def first_pins
+    @first_shot.pins
+  end
+
+  def second_pins
+    @second_shot&.pins
   end
 
   def total_pins
@@ -18,9 +24,9 @@ class Frame
   def point(next_frame, after_next_frame)
     total_pins +
       if strike?
-        next_frame.first_shot.pins + (next_frame.second_shot&.pins || after_next_frame.first_shot.pins)
+        next_frame.first_pins + (next_frame.second_pins || after_next_frame.first_pins)
       elsif spare?
-        next_frame.first_shot.pins
+        next_frame.first_pins
       else
         0
       end
@@ -29,7 +35,7 @@ class Frame
   private
 
   def strike?
-    first_shot.pins == Shot::PINS_PER_FRAME
+    @first_shot.pins == Shot::PINS_PER_FRAME
   end
 
   def spare?
